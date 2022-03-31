d4694d36cdcbc22bac834a88b42224d0c4d8cda8
milly <milly.tang@chiefclouds.com>
2022-03-31 19:44:38 +0800

    feat: add Scanner

---
+++ lox/Scanner.java
@@ -0,0 +1,64 @@
+package com.craftinginterpreters.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.craftinginterpreters.lox.TokenType.*; 
+
+class Scanner {
+  private final String source;
+  private int start = 0;
+  private int current = 0;
+  private int line = 1;
+  private final List<Token> tokens = new ArrayList<>();
+
+  Scanner(String source) {
+    this.source = source;
+  };
+  private boolean isAtEnd() {
+    return current >= source.length();
+  }
+  // The advance() method consumes the next character in the source file and returns it.
+  private char advance() {
+    return source.charAt(current++);
+  }
+  // Where advance() is for input, addToken() is for output. 
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
+  }
+  List<Token> scanTokens() {
+    while (!isAtEnd()) {
+      // We are at the beginning of the next lexeme.
+      start = current;
+      scanToken();
+    }
+
+    tokens.add(new Token(EOF, "", null, line));
+    return tokens;
+  }
+  private void scanToken() {
+    char c = advance();
+    switch (c) {
+      case '(': addToken(LEFT_PAREN); break;
+      case ')': addToken(RIGHT_PAREN); break;
+      case '{': addToken(LEFT_BRACE); break;
+      case '}': addToken(RIGHT_BRACE); break;
+      case ',': addToken(COMMA); break;
+      case '.': addToken(DOT); break;
+      case '-': addToken(MINUS); break;
+      case '+': addToken(PLUS); break;
+      case ';': addToken(SEMICOLON); break;
+      case '*': addToken(STAR); break; 
+      default:
+        Lox.error(line, "Unexpected character.");
+        break;
+    }
+  }
+}
